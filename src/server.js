4ce8ac5cc879816b8ad91aae579d64309740aba4
Gabriel Farias <farias2752@gmail.com>
2023-10-17 09:50:38 -0300

    Query params

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+const { request } = require("express");
+const { response } = require("express");
 const express = require ("express");
 
 const App = express();
 
+//Route params
 App.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params
+    const { id, user } = request.params;
 
     response.send(`
-    Mensagem ID: ${id}.
-    Para o usuário: ${user}
+        Mensagem ID: ${ id }.
+        Para o usuário: ${ user }.
     `)
 });
+
+//Query Params
+App.get("/user", (request, response) => {
+    const { page, limit} = request.query;
+
+    response.send(`
+        Páginas: ${ page }.
+        Limite: ${ limit }.
+    `);
+})
 
 const PORT = 3333;
 App.listen(PORT, () => console.log(`Server is running on Port ${PORT} `));
