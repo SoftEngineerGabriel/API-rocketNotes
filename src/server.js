4c6c5dbf52a01183c5299c15a3f4a1021bc3f4b3
Gabriel Farias <farias2752@gmail.com>
2023-10-17 08:40:34 -0300

    M

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require ("express");
 
 const App = express();
 
+App.get("/", (request, response) => {
+    response.send("Hello word!");
+});
+
 const PORT = 3333;
 App.listen(PORT, () => console.log(`Server is running on Port ${PORT} `));
