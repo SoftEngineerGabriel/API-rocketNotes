86141dad8b48756207825ba5faddc1efc85e5ce0
Gabriel Farias <farias2752@gmail.com>
2023-10-18 09:21:47 -0300

    Tratando erros com o AppError

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError")
+
 class UserController {
     create(request, response) {
         const { name, email, password } = request.body;
+
+
+        if(!name) {
+            throw new AppError("Name is obligatory!");
+        }
 
         response.status(201).json({ name, email, password });
     }
