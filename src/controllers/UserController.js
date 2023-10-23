3cc99ac5cde279f26ac58fe614e4d5b2377a71b8
Gabriel Farias <farias2752@gmail.com>
2023-10-23 07:05:23 -0300

    SELECT

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,15 +1,19 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite");
 
 class UserController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-        if(!name) {
-            throw new AppError("Name is obligatory!");
+        if(checkUserExists) {
+            throw new AppError("User already exists");
         }
 
-        response.status(201).json({ name, email, password });
+        return response.status(201).json();
+
     }
 }
 
