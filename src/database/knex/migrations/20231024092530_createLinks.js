7ec23529e7a99ed523b68b0e05f36be220651d2a
Gabriel Farias <farias2752@gmail.com>
2023-10-24 07:57:19 -0300

    Cadastrando notas

--- src/database/knex/migrations/20231024092530_createLinks.js
+++ src/database/knex/migrations/20231024092530_createLinks.js
@@ -1,10 +1,10 @@
-exports.up = knex =>
-  knex.schema.createTable("links", table => {
-    table.increments("id")
-    table.text("url");
-    
-    table.integer("node_id").references("id").inTable("notes").onDelete("CASCADE");
-    table.timestamp('created_at').default(knex.fn.now())
-})
-
-exports.down = knex => knex.schema.dropTable("links")
+exports.up = knex => knex.schema.createTable("links", table => {
+    table.increments("id");
+    table.text("url").notNullable();
+  
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
+  
+    table.timestamp("created_at").default(knex.fn.now());
+  });
+  
+  exports.down = knex => knex.schema.dropTable("links");
