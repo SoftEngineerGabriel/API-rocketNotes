242e2c18af7dd386f26c736014565ba674bf3e2b
Gabriel Farias <farias2752@gmail.com>
2023-11-21 06:35:40 -0300

    Rota de autenticação

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express")
 
 const usersRouter = require("./user.routes");
 const notesRouter = require("./notes.routes");
+const sessionRouter = require("./session.routes");
 const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
